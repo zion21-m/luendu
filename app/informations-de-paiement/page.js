91f2014e59a58f8b9f7e7d63b54af34949cae4e8
Merdie Mulaba <merdiemlb@gmail.com>
2024-05-06 16:31:30 +0100

    update page

--- app/informations-de-paiement/page.js
+++ app/informations-de-paiement/page.js
@@ -7,59 +7,50 @@ const Page = () => {
       <div className="flex flex-col gap-3">
         <p>
           Félicitations ! Vous avez franchi une étape importante dans votre
-          expérience avec notre plateforme. En ajoutant vos méthodes de
-          paiement, vous vous assurez une navigation plus fluide et sécurisée
-          lors de vos transactions.
+          expérience sur notre plateforme. En ajoutant le paiement, vous
+          confirmerez votre brique sur{" "}
+          <span className="font-semibold italic text-blue-600">
+            le mur de l&apos;honneur.
+          </span>{" "}
+          Vous disposez de 5 jours pour la confirmation de votre place
         </p>{" "}
         <p>
-          Désormais, vous avez la liberté de choisir parmi une gamme de méthodes
-          de paiement flexibles, adaptées à vos préférences et à vos besoins :
+          Ainsi, pour cette étape, vous avez la liberté de choisir parmi une
+          gamme de méthodes de paiement flexibles, adaptées à vos préférences et
+          à vos besoins :
         </p>
         <p>
           <span className="font-semibold">Proforma avec compte bancaire :</span>{" "}
-          Cette méthode vous permet de régler vos achats par virement bancaire.
-          Une fois votre commande confirmée, vous recevrez une facture proforma
-          avec les détails nécessaires pour effectuer le paiement via votre
-          compte bancaire.{" "}
+          Cette méthode vous permet de confirmer votre paiement par virement ou
+          depôt bancaire. Une fois le paiement effectué, veuillez nous envoyer
+          une copie de votre bordereau pour que nous confirmions votre paiement
+          .{" "}
         </p>
         <p>
           <span className="font-semibold">Payer à la fondation :</span>
-          Vous avez également la possibilité de régler vos transactions en
-          personne à notre fondation. Une fois que vous avez sélectionné cette
-          option de paiement, vous pouvez vous rendre à nos locaux et effectuer
-          le règlement sur place.
+          Vous avez également la possibilité de régler votre paiement en
+          personne à notre fondation, size à l&apos;appartement 408, Immeuble
+          Skyview, Commune de la Gombe, à Kinshasa. Référence Restaurant chez
+          Paul et salle Romeo Golf. Une fois le montant payé dans nos locaux,
+          vous recevrez une confirmation et votre paiement sera enregistré sur
+          notre site
         </p>{" "}
         <p>
           <span className="font-semibold">
-            Récupération dans vos locaux (avec frais de 50 $)
+            Récupération dans vos locaux (avec frais de 10 $)
           </span>
           : Si vous préférez, nous pouvons également organiser la récupération
           de votre paiement en envoyant quelqu&apos;un à votre emplacement.
           Cette option est pratique si vous souhaitez régler en espèces ou par
-          chèque. Veuillez noter qu&apos;un frais de transport de 50 $ sera
+          chèque. Veuillez noter qu&apos;un frais de transport de 10 $ sera
           ajouté pour couvrir les frais de déplacement. Votre sécurité est notre
           priorité absolue.
         </p>{" "}
         <p>N.B :</p>
         <p>
-          Soyez assuré(e) que vos informations de paiement sont cryptées et
-          protégées par des mesures de sécurité avancées. Nous veillons à ce que
-          vos données personnelles restent confidentielles et sécurisées à tout
-          moment.
-        </p>
-        <p>
-          Grâce à vos méthodes de paiement enregistrées, vous pourrez effectuer
-          des transactions en toute tranquillité d&apos;esprit, que ce soit pour
-          des achats réguliers ou des services additionnels. Plus besoin de
-          saisir manuellement vos informations à chaque fois, votre compte est
-          désormais prêt à l&apos;emploi pour des transactions rapides et
-          efficaces.
-        </p>
-        <p>
-          N&apos;hésitez pas à explorer les différentes options de paiement
-          disponibles dans votre profil utilisateur. Vous pouvez les gérer, les
-          modifier ou en ajouter de nouvelles à tout moment, selon vos besoins
-          évolutifs.
+          Soyez assuré(e) que vos informations de paiement sont protégées et
+          Nous veillons à ce que vos données personnelles restent
+          confidentielles et sécurisées à tout moment.
         </p>
         <p>
           Nous vous remercions de votre confiance et sommes ravis de vous
